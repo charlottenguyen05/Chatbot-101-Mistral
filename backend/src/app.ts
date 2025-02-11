0aff3c80d4d7834fab4b6fd3fb304c7db7cf889c
charlottenguyen05 <nhinguyenngoclinh2005@gmail.com>
2025-02-11 02:03:18 +0100

    ready to deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET))
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.VITE_APP_BACKEND_BASEURL,
         credentials: true,
     })
 )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import AuthProvider from "./providers/AuthProvider.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = `${process.env.VITE_APP_BACKEND_BASEURL}/api` || "http://localhost:3000/api";  // Ti nua phai sua thanh dia chi deploy
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
